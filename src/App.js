b69b62a0a616e87f7d7db14f37fc993f0b7abdc9
Kanika Goel <kanikagoel688@gmail.com>
2025-10-08 23:57:39 +0530

    translate button added

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
                 element={
                 <TextForm
                 showAlert={showAlert}
-                heading="Enter the text to analyze below"
+                heading="Enter the Text to Analyze Below"
                 mode={mode}
               />
             }
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,126 +1,173 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import translate from "translate";
+
+translate.engine = "google";
+translate.key = null;
+
+
+
 
 export default function TextForm(props) {
-  const [text, setText] = useState("");
-  const handleUpClick = () => {
-    console.log("Upper Case was clicked");
-    let newText = text.toUpperCase();
-    setText(newText);
-    props.showAlert("Converted to UpperCase","success");
-  };
 
-  const handleDownClick = () => {
-    console.log("DownClick was Clicked");
-    let newText = text.toLowerCase();
-    setText(newText);
-    props.showAlert("Converted to LowerCase","success");
-  };
-
-  const handleClearClick = () => {
-    console.log("Clear was Clicked");
-    let newText = "";
-    setText(newText);
-    props.showAlert("Cleared","success");
-  };
-
-  const handleCopy = () => {
-    console.log("copy button is being clicked");
-    var text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    props.showAlert("Copied","success");
-  };
-
-  const handleExtraSpaces = () => {
-    let newText = text.split(/[ ]+/);
-    setText(newText.join(" "));
-    props.showAlert("Extra Spaces are removed","success");
-  };
-
-  const handleOnChange = (event) => {
-    console.log("On Change was clicked");
-    setText(event.target.value);
-  };
-
-  function handleTextToSpeech() {
-    const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance(text);
-    synth.speak(utterance);
-    return undefined;
-  }
-
-  function handleSpeechToText() {
-    const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new speechRecognition();
-    recognition.lang = 'en-US';
-    recognition.start();
-    recognition.onresult = function(event) {
-      const transcript = event.results[0][0].transcript;
-      setText(transcript);
+    const [text, setText] = useState("");
+    const handleUpClick = () => {
+        console.log("Upper Case was clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase", "success");
     };
-    return undefined;
-  }
 
-  return (
-    <>
-      <div
-        className="container"
-        style={{
-          
-          color: props.mode === "dark" ? "white" : "#042743",
-        }}
-      >
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
+    const handleDownClick = () => {
+        console.log("DownClick was Clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase", "success");
+    };
+
+    const handleClearClick = () => {
+        console.log("Clear was Clicked");
+        let newText = "";
+        setText(newText);
+        props.showAlert("Cleared", "success");
+    };
+
+    const handleCopy = () => {
+        console.log("copy button is being clicked");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied", "success");
+    };
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra Spaces are removed", "success");
+    };
+
+    const handleOnChange = (event) => {
+        console.log("On Change was clicked");
+        setText(event.target.value);
+    };
+
+    function handleTextToSpeech() {
+        const synth = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance(text);
+        synth.speak(utterance);
+        return undefined;
+    }
+
+    function handleSpeechToText() {
+        const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        const recognition = new speechRecognition();
+        recognition.lang = 'en-US';
+        recognition.start();
+        recognition.onresult = function (event) {
+            const transcript = event.results[0][0].transcript;
+            setText(transcript);
+        };
+        return undefined;
+    }
+
+    const handleTranslate = async(langCode, langName) => {
+        if (text.trim() === "") {
+            props.showAlert("Please enter some text to translate", "warning");
+            return;
+        }
+
+        try{
+            const translatedText = await translate(text, {to: langCode});
+            setText(translatedText);
+            props.showAlert(`Text translated to ${langName}`, "success");
+        }
+        catch(error){
+            props.showAlert(`Error translating text to ${langName}: ${error.message}`, "danger");
+        }
+    }
+
+    return (
+        <>
+            <div
+                className="container"
+                style={{
+
+                    color: props.mode === "dark" ? "white" : "#042743",
+                }}
+            >
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
           <textarea
-            className="form-control"
-            value={text}
-            style={{
-              backgroundColor: props.mode === "dark" ? "grey" : "white",
-              color: props.mode === "dark" ? "white" : "grey",
-            }}
-            onChange={handleOnChange}
-            id="myBox"
-            rows="10"
+              className="form-control"
+              value={text}
+              style={{
+                  backgroundColor: props.mode === "dark" ? "grey" : "white",
+                  color: props.mode === "dark" ? "white" : "grey",
+              }}
+              onChange={handleOnChange}
+              id="myBox"
+              rows="10"
           ></textarea>
-        </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
-          Convert to UpperCase
-        </button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleDownClick}>
-          Convert to LowerCase
-        </button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleClearClick}>
-          Clear
-        </button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleCopy}>
-          Copy
-        </button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleExtraSpaces}>
-          Remove Extra Spaces
-        </button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleSpeechToText}>Speech to Text</button>
-        &nbsp;
-        <button className="btn btn-primary" onClick={handleTextToSpeech}>Text to Speech</button>
-      </div>
-      <div className="container my-4" style={{
-          
-          color: props.mode === "dark" ? "white" : "#042743",
-        }}>
-        <h1>Your text summary</h1>
-        
-        <p>{text === "" ? '0': text.trim().split(/\s+/).length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length}Minutes to read</p>
-        <p>{text.split(".").length - 1} lines</p>
-        <p>{text.split("\n").length - 1} paragraph</p>
-        <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textbook above to preview it here:"}</p>
-      </div>
-    </>
-  );
+                </div>
+                <button className="btn btn-primary" onClick={handleUpClick}>
+                    Convert to UpperCase
+                </button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleDownClick}>
+                    Convert to LowerCase
+                </button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleClearClick}>
+                    Clear
+                </button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleCopy}>
+                    Copy
+                </button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleExtraSpaces}>
+                    Remove Extra Spaces
+                </button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleSpeechToText}>Speech to Text</button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handleTextToSpeech}>Text to Speech</button>
+                &nbsp;
+                    <button className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+                            aria-expanded="false">Translate
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <button className="dropdown-item" onClick={() => handleTranslate('es', 'Spanish')}>Spanish
+                            </button>
+                        </li>
+                        <li>
+                            <button className="dropdown-item" onClick={() => handleTranslate('fr', 'French')}>French
+                            </button>
+                        </li>
+                        <li>
+                            <button className="dropdown-item" onClick={() => handleTranslate('de', 'German')}>German
+                            </button>
+                        </li>
+                        <li>
+                            <button className="dropdown-item" onClick={() => handleTranslate('hi', 'Hindi')}>Hindi
+                            </button>
+                        </li>
+                    </ul>
+            </div>
+            <div className="container my-4" style={{
+
+                color: props.mode === "dark" ? "white" : "#042743",
+            }}>
+                <h1>Your Text Summary</h1>
+
+                <p>{text.length} characters</p>
+                <p>{text === "" ? '0' : text.trim().split(/\s+/).length} words</p>
+                <p>{text.split(".").length - 1} lines</p>
+                <p>{text.split("\n").length - 1} paragraph</p>
+                <p>{0.008 * text.split(" ").length} minutes to read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter something in the textbook above to preview it here:"}</p>
+            </div>
+        </>
+    );
 }
